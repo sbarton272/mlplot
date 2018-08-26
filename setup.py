376de642937b6c51f776f9d2c50c4af65414168d
Spencer Barton <sbarton272@gmail.com>
2018-08-26 16:56:59 -0700

    Package starter

---
+++ setup.py
@@ -0,0 +1,10 @@
+"""Package setup"""
+from distutils.core import setup
+
+setup(
+    name='mlplot',
+    version='1.0.0',
+    packages=['mlplot',],
+    license='MIT License',
+    long_description=open('README.txt').read(),
+)
