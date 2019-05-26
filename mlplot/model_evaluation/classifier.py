55609ed37ff598bfbd089d0860d41ab5d09b4901
Spencer Barton <sbarton272@gmail.com>
2019-05-26 15:39:01 -0700

    Add arg checks

--- mlplot/model_evaluation/classifier.py
+++ mlplot/model_evaluation/classifier.py
@@ -12,8 +12,7 @@ class ClassifierEvaluation(ModelEvaluation):
     y_true : list or 1d numpy array with elements 0 or 1
              A collection of the true labels which can be any two values
     y_pred : list or 1d numpy array with elements 0.0 to 1.0
-             A collection of the prediction probabilities.
-             It is assumed that the first class in classes is the
+             A collection of the prediction probabilities of class value 1.
     class_names : list with two string elements
                   These are the names of the two classes and they are used in plots.
                   The first string maps to the 0 class and the second to the 1 class.
@@ -24,12 +23,20 @@ class ClassifierEvaluation(ModelEvaluation):
     def __init__(self, y_true, y_pred, class_names, model_name):
         super(y_true=y_true, y_pred=y_pred, model_name=model_name)
 
-        # Check that y_true has only 0 and 1
+        # Check y_true values
         true_values = np.sort(np.unique(y_true))
         if not np.equal(true_values, np.array([0, 1])).all()
-            raise InvalidArgument('')
+            raise InvalidArgument('y_true must contain only values of 0 or 1')
 
-    def _validate
+        # Check y_pred values
+        if ((a < 0.0) | (a > 1.0)).any():
+            raise InvalidArgument('y_pred must contain only values between [0.0, 1.0]')
+
+        # Check class names
+        if len(class_names) != 2:
+            raise InvalidArgument('class_names must contain two class names (strings)')
 
     def __repr__(self):
         return '{}(0:{}, 1:{})'.format(self.model_name, self.class_names[0], self.class_names[1])
+
+
